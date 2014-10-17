a3cc8c3de5eec330772e184e647ba591fea18bd1
weatherdave <weatherdave1@gmail.com>
2014-10-16 23:46:53 -0400

    Final Update 10162014
    Just final additions of code/comments

--- Board.cpp
+++ Board.cpp
@@ -6,3 +6,15 @@ public:
 	void solutionCheck();
 	void displaySolution();
 };
+
+void Board::createBoard(){
+	//possibly to be done using QT. If not, see below:
+	/*
+	sample 1st row of sudoku board (when completed). each letter will initially be a nubmer or a space. There are 9! different positions of the top-left square, and therefore 9!
+	configurations of the square when all #s filled in.
+		___________________
+		|a b c|d e f|g h i|
+		|d e f|g h i|a b c|
+		|g_h_i|a_b_c|d_e_f|
+		*/
+}
--- sudoku.cpp
+++ sudoku.cpp
@@ -2,12 +2,21 @@
 #include <iostream>
 using namespace std;
 
+class Sudoku : public Board{
+public:
+	void initialize();
+	void solve();
+	void display();
+};
+
 void Sudoku::initialize(){
 	cout << "Initializing............";
+	createBoard();
 	return;
 }
 
 void Sudoku::solve(){
+	displaySolution();
 	return;
 }
 
